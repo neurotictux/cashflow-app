a4387f3d6c7e691b0ac4d631035f21cc93fbadf3
Gabriel Santos <gabrielsantoscimol@gmail.com>
2019-05-25 05:09:28 -0300

    CrossCutting em typescript

---
+++ mod-cross-cutting/src/util/date.ts
@@ -0,0 +1,24 @@
+/**
+ * Retorna a string de uma data no formato do parâmetro informado.
+ * @example
+ *  toDateFormat(new Date(), 'dd/MM/yy')
+ * @param {Date} date Data a ser formatada
+ * @param {String} format Formato em que a data deve ser retornada
+ * @returns {String} Data formatada
+ */
+export const toDateFormat = (date: Date, format: string): string => {
+  if (!date || !format || date.toString() === 'Invalid Date')
+    return 'Invalid Date'
+  const year = String(date.getFullYear())
+  let month = (date.getMonth() + 1) + ''
+  const day = date.getDate()
+  month = (Number(month) > 9 ? '' : '0') + month
+  switch (format) {
+    case 'dd/MM/yy':
+      return `${(day > 9 ? '' : '0') + day}/${month}/${year.substring(2, 4)}`
+    case 'MM/yyyy':
+      return `${month}/${year}`
+    default:
+      return `${(day > 9 ? '' : '0') + day}/${month}/${year}`
+  }
+}
---
+++ mod-cross-cutting/src/util/errors.ts
@@ -0,0 +1,22 @@
+/**
+ * Tipos de erros a serem tratados
+ */
+export const ErrorTypes = {
+  VALIDATION: 1
+}
+
+/**
+ *
+ * @param {String} message
+ */
+export const throwValidationError = (message: string) => { throw { message, type: ErrorTypes.VALIDATION } }
+
+/**
+ *
+ * @param {String} serviceName
+ * @param {String} functionName
+ */
+export const throwRepositoryError = (serviceName: string, functionName: string) => {
+  const msg = `Parameter repository in ${serviceName}  must have a function ${functionName} that returns a promise.`
+  throw new Error(msg)
+}
